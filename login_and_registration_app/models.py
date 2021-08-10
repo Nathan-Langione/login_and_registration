d3f5381eab2083a821afcd566f73d76a00d2b80a
Nathan Langione <nlangione@wayfair.com>
2021-08-10 08:21:13 -0400

    Fixed login validation with new function

--- login_and_registration_app/models.py
+++ login_and_registration_app/models.py
@@ -23,13 +23,39 @@ class UserManager(models.Manager):
         bad_email = User.objects.filter(email = postData['email'])
         if len(bad_email) > 0:
                 errors["email"] = "Please use a different email"
-
+        t_date = date.today()
+        r_date =postData['birth_date']
+        try:
+            fr_date = datetime.strptime(r_date,'%Y-%m-%d')
+            if fr_date.year > t_date.year:
+                errors["birth_date"] = "Birthday should be in the past"
+            elif fr_date.year == t_date.year and fr_date.month > t_date.month:
+                errors["birth_date"] = "Birthday should be in the past"
+            elif fr_date.year == t_date.year and fr_date.month == t_date.month and fr_date.day > t_date.day:
+                errors["birth_date"] = "Birthday should be in the past"
+        except:
+            errors["birth_date"] = "Birthday can't be blank"
         return errors
 
+    def login_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['password']) < 8:
+            errors["password"] = "Password  should be at least 8 characters"
+        email_regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if(re.search(email_regex,postData['email'])):
+            pass
+        else:
+            errors["email"] = "Please format your email correctly"
+        bad_email = User.objects.filter(email = postData['email'])
+        if len(bad_email) > 0:
+                errors["email"] = "Please use a different email"
+        return errors
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    birth_date = models.DateField(blank=True, null=True)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- login_and_registration_app/views.py
+++ login_and_registration_app/views.py
@@ -9,7 +9,8 @@ def index(request):
 
 def success(request):
     try:
-        User.objects.filter(id=request.session['userid'])
+        if len(User.objects.filter(id=request.session['userid'])) > 0:
+            pass
     except:
         return redirect("/")
     one_user = User.objects.get(id=request.session['userid'])
@@ -30,22 +31,44 @@ def register(request):
             last_name = request.POST.get('last_name')
             email = request.POST.get('email')
             password = request.POST['password']
+            birth_date = request.POST['birth_date']
             pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-            print(pw_hash)
             new_user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=pw_hash)
-            new_user.save()
             request.session['userid'] = new_user.id
             return redirect("/success")
 
 
+""" def login(request):
+    if request.method=="POST":
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect("/")
+    else:
+        user = User.objects.filter(email=request.POST['email'])
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id
+                return redirect('/success')
+    return redirect("/") """
+
 def login(request):
-    user = User.objects.filter(email=request.POST['email'])
-    if user:
-        logged_user = user[0]
-        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
-            request.session['userid'] = logged_user.id
-            return redirect('/success')
-    return redirect("/")
+    if request.method=="POST":
+        errors = User.objects.login_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect("/")
+        else:
+            user = User.objects.filter(email=request.POST['email'])
+            if user:
+                logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id
+                return redirect('/success')
+        return redirect("/")
 
 def logout(request):
     request.session.flush()
